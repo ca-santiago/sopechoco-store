de894370e2ed10227eee694d1f1a63946cfbd872
ca-santiago <casantiago.me@gmail.com>
2024-10-29 18:57:50 -0600

    add orders mock

---
+++ src/mocks/orders.ts
@@ -0,0 +1,58 @@
+import { Order } from "@/types";
+
+const testOrders: Order[] = [
+    {
+        "id": "cm2uvj8xv0000p3xwbziepe17",
+        "publicId": "MXK-572",
+        "status": "ACCEPTED",
+        "total": 162,
+        "cartDetails": "[{\"itemId\":\"083ddece-e730-4a81-94b2-9f5a94f80287\",\"productId\":\"ee280eab-449d-4838-9213-eef12fd87359\",\"addedExtras\":[\"sopes-guisos-1:3276f5b7-5d63-4667-a9ba-57cde8b47515:1\",\"sopes-guisos-1:2fc7a4e3-1dcc-4ef7-9cb4-c85ac506bfc2:1\"],\"quantity\":2}]",
+        "createdAt": "2024-10-29T20:02:05.526Z",
+        "updatedAt": "2024-10-30T00:53:52.867Z"
+    },
+    {
+        "id": "cm2ux25wr0001p3xwsd3ktugk",
+        "publicId": "CBY-176",
+        "status": "REJECTED",
+        "total": 291,
+        "cartDetails": "[{\"itemId\":\"5483d964-8d92-4c40-afa5-c6a592cc66fe\",\"productId\":\"ee280eab-449d-4838-9213-eef12fd87359\",\"addedExtras\":[\"sopes-guisos-1:986965e5-759f-4c2e-b88b-73511983eabd:1\",\"sopes-guisos-1:3276f5b7-5d63-4667-a9ba-57cde8b47515:1\"],\"quantity\":3},{\"itemId\":\"6ce5128f-c4a1-4e79-93a3-65196c2c0843\",\"productId\":\"90637cf2-89b6-45fd-9880-d338735447ee\",\"addedExtras\":[\"tacos-guisos-1:3276f5b7-5d63-4667-a9ba-57cde8b47515:1\",\"tacos-guisos-1:b3c66df9-df74-438d-b283-35e0c307b0ee:1\"],\"quantity\":4}]",
+        "createdAt": "2024-10-29T20:44:48.221Z",
+        "updatedAt": "2024-10-30T00:14:08.723Z"
+    },
+    {
+        "id": "cm2uy48e50002p3xwzl5xriy2",
+        "publicId": "URL-604",
+        "status": "PENDING",
+        "total": 405,
+        "cartDetails": "[{\"itemId\":\"887405c8-ac6b-4905-9f00-be495422dde1\",\"productId\":\"ee280eab-449d-4838-9213-eef12fd87359\",\"addedExtras\":[\"sopes-guisos-1:3276f5b7-5d63-4667-a9ba-57cde8b47515:1\",\"sopes-guisos-1:986965e5-759f-4c2e-b88b-73511983eabd:1\"],\"quantity\":5}]",
+        "createdAt": "2024-10-29T21:14:24.508Z",
+        "updatedAt": "2024-10-30T00:53:47.643Z"
+    },
+    {
+        "id": "cm2v4m64t0003p3xwtvr03one",
+        "publicId": "WJF-481",
+        "status": "PENDING",
+        "total": 243,
+        "cartDetails": "[{\"itemId\":\"31896947-bccb-4506-94d4-55fb4872be83\",\"productId\":\"ee280eab-449d-4838-9213-eef12fd87359\",\"addedExtras\":[\"sopes-guisos-1:3276f5b7-5d63-4667-a9ba-57cde8b47515:1\"],\"quantity\":3}]",
+        "createdAt": "2024-10-30T00:16:18.933Z",
+        "updatedAt": "2024-10-30T00:53:46.931Z"
+    },
+    {
+        "id": "cm2v4nayd0004p3xwn1e78bu8",
+        "publicId": "RSO-798",
+        "status": "ACCEPTED",
+        "total": 12,
+        "cartDetails": "[{\"itemId\":\"5c0f487f-4419-424a-b0a5-cb1c0f68fc59\",\"productId\":\"90637cf2-89b6-45fd-9880-d338735447ee\",\"addedExtras\":[\"tacos-guisos-1:3276f5b7-5d63-4667-a9ba-57cde8b47515:4\",\"tacos-guisos-1:74465580-4b4b-4130-91c7-9c3e3adfa827:4\",\"tacos-bebidas-1:dfe08bad-94a8-4476-9331-5dd68e5882ef:1\"],\"quantity\":1}]",
+        "createdAt": "2024-10-30T00:17:11.988Z",
+        "updatedAt": "2024-10-30T00:50:50.768Z"
+    },
+    {
+        "id": "cm2v58ba70005p3xwycdqczig",
+        "publicId": "DNQ-508",
+        "status": "PENDING",
+        "total": 318,
+        "cartDetails": "[{\"itemId\":\"332e3a6e-c10e-4cb3-9736-c53d461d3830\",\"productId\":\"90637cf2-89b6-45fd-9880-d338735447ee\",\"addedExtras\":[\"tacos-guisos-1:3276f5b7-5d63-4667-a9ba-57cde8b47515:1\",\"tacos-guisos-1:b3c66df9-df74-438d-b283-35e0c307b0ee:4\",\"tacos-bebidas-1:dcffe503-e809-4c6f-acf2-56e36595aec8:1\"],\"quantity\":1},{\"itemId\":\"38633af6-34ce-4481-ab5d-36a6319b7651\",\"productId\":\"ee280eab-449d-4838-9213-eef12fd87359\",\"addedExtras\":[\"sopes-guisos-1:986965e5-759f-4c2e-b88b-73511983eabd:1\"],\"quantity\":1},{\"itemId\":\"aba01fc3-74c8-4c8d-9b7a-fb140c1f47c1\",\"productId\":\"8418032c-4042-4d4d-a813-95d332648d05\",\"addedExtras\":[],\"quantity\":3}]",
+        "createdAt": "2024-10-30T00:33:31.541Z",
+        "updatedAt": "2024-10-30T00:33:31.541Z"
+    }
+];
